5ea3ad6d3049c9cbc6ea36657ad05d1e85a597cc
ttg96 <ttg96@users.noreply.github.com>
2021-04-29 11:32:25 +0100

    fixed menu quitting bug

--- Assets/UI/Scripts/SceneController.cs
+++ Assets/UI/Scripts/SceneController.cs
@@ -17,6 +17,7 @@ public class SceneController : MonoBehaviour
     {
         optionsManager = FindObjectOfType<OptionsManager>();
         if(optionsManager) optionsManager.InitialiseCameras();
+        Time.timeScale = 1;
     }
 
     public void ResetLevel()
